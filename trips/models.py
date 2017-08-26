c7102bba8db17cfe4fa607a4b5d97041d20eede0
Derek Covey <derek.covey@gmail.com>
2017-08-26 11:48:41 -0700

    Create User and Vehicle models

--- trips/models.py
+++ trips/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50)
+
+class Vehicle(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.CharField(max_length=4)
+    color = models.CharField(max_length=20)
+    lic_plate_num = models.CharField(max_length=20)
+    lic_plate_st = models.CharField(max_length=2)
