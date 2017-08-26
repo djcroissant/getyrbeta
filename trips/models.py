a38b3a30c3b0bcb88329c601b4425fe86b5962ea
Derek Covey <derek.covey@gmail.com>
2017-08-26 12:33:27 -0700

    Add str method to models

--- trips/models.py
+++ trips/models.py
@@ -5,6 +5,9 @@ class User(models.Model):
     last_name = models.CharField(max_length=50)
     nickname = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
 class Vehicle(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     make = models.CharField(max_length=50)
@@ -13,3 +16,6 @@ class Vehicle(models.Model):
     color = models.CharField(max_length=20)
     lic_plate_num = models.CharField(max_length=20)
     lic_plate_st = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.year + ' ' + self.make + ' ' + self.model
