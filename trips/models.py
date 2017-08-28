73bc65c47bfe5c27b29229728218b73d62a0cbde
Derek Covey <derek.covey@gmail.com>
2017-08-28 12:33:46 -0700

    Create Trip model

--- trips/models.py
+++ trips/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
+class Trip(models.Model):
+    th_lat = models.CharField(max_length = 20)
+    th_lon = models.CharField(max_length = 20)
+    title = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.title
+
 class User(models.Model):
+    trips = models.ManyToManyField(Trip)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     nickname = models.CharField(max_length=50)
