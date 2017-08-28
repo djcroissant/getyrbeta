e98f83fed05b0a52d6ed9cc9ada3b571976134ba
Derek Covey <derek.covey@gmail.com>
2017-08-28 12:13:39 -0700

    Create initial User and Vehicle views

--- trips/urls.py
+++ trips/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
+
 from . import views
 
+app_name = 'trips'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^profile/(?P<user_id>[0-9]+)/$', views.user_profile, name='user_profile'),
+    url(r'^vehicle/(?P<vehicle_id>[0-9]+)/$', views.vehicle_detail, name='vehicle_detail'),
 ]
--- trips/views.py
+++ trips/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.http import Http404
+
+from .models import User, Vehicle
+
 
 def index(request):
     return HttpResponse("Hi, you're at the trips index")
+
+def user_profile(request, user_id):
+    user = User.objects.get(pk = user_id)
+    vehicle_list = user.vehicle_set.all()
+    context = {
+        'user': user,
+        'vehicle_list': vehicle_list,
+    }
+    return render(request, 'users/profile.html', context)
+
+def vehicle_detail(request, vehicle_id):
+    vehicle = get_object_or_404(Vehicle, pk=vehicle_id)
+    return render(request, 'vehicles/detail.html', {'vehicle': vehicle})
