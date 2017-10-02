fb337307c14a74d6ef9afd7e6270a78766e5d6d6
Derek Covey <derek.covey@gmail.com>
2017-10-02 13:26:18 -0700

    Update trips/urls.py

--- tripplan/trips/urls.py
+++ tripplan/trips/urls.py
@@ -7,10 +7,8 @@ app_name = 'trips'
 
 urlpatterns = [
     url(r'^$', views.TripList.as_view(), name='trip_list'),
-    url(r'^trip/(?P<pk>[0-9]+)/$', views.TripView.as_view(), name='trip_detail'),
-    url(r'^trip/addtrip/$',
+    url(r'^(?P<pk>[0-9]+)/$', views.TripView.as_view(), name='trip_detail'),
+    url(r'^create/$',
         views.TripCreateView.as_view(), name='trip_create'),
-    url(r'^trip/(?P<pk>[0-9]+)/$',
-        views.TripView.as_view(), name='trip_detail'),
     url(r'^notifications/$', views.notifications, name='notifications'),
 ]
