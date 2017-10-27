66665d2ff12cb1f527f46775456f074408bc8138
Derek Covey <derek.covey@gmail.com>
2017-10-27 11:19:14 -0700

    --amend

--- tripplan/trips/templatetags/trip_extras.py
+++ tripplan/trips/templatetags/trip_extras.py
@@ -7,14 +7,3 @@ register = template.Library()
 @register.filter
 def index(List, i):
     return List[int(i)]
-
-@register.filter
-def convert_location(location):
-    if location == TripLocation.BEGIN:
-        return 'trailhead'
-    elif location == TripLocation.END:
-        return 'endpoint'
-    elif location == TripLocation.OBJECTIVE:
-        return 'objective'
-    elif location == TripLocation.CAMP:
-        return 'camp'
