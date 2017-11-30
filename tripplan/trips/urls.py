ac4bd829691414dce573e04a24d66214944a3743
Derek Covey <derek.covey@gmail.com>
2017-11-30 09:25:16 -0800

    Add urlconf

--- tripplan/trips/urls.py
+++ tripplan/trips/urls.py
@@ -6,20 +6,33 @@ from . import views
 app_name = 'trips'
 
 urlpatterns = [
+    # Trips
     url(r'^$', views.TripListView.as_view(), name='trip_list'),
     url(r'^(?P<pk>[0-9]+)/$',
         views.TripDetailView.as_view(), name='trip_detail'),
     url(r'^create/$', views.TripCreateView.as_view(), name='trip_create'),
+
+    # Locations
     url(r'^(?P<trip_id>[0-9]+)/create/(?P<location_type>[\w]+)/$',
         views.LocationCreateView.as_view(), name='location_create'),
     url(r'^(?P<trip_id>[0-9]+)/edit/(?P<location_type>[\w]+)/(?P<pk>[0-9]+)/$',
         views.LocationEditView.as_view(), name='location_edit'),
     url(r'^(?P<trip_id>[0-9]+)/delete/(?P<location_type>[\w]+)/(?P<pk>[0-9]+)/$',
         views.LocationDeleteView.as_view(), name='location_delete'),
+
+    # Notifications
     url(r'^notifications/$',
         views.NotificationListView.as_view(), name='notifications'),
+
+    # Emergency Info
+    url(r'^emergency_info/$',
+        views.EmergencyInfoListView.as_view(), name='emergency_info'),
+
+    # Members
     url(r'^(?P<pk>[0-9]+)/members/$',
         views.TripMemberListView.as_view(), name='trip_members'),
+
+    # AJAX requests:
     url(r'^ajax/user_exists/$',
         views.CheckUserExistsView.as_view(), name='user_exists'),
     url(r'^ajax/add_trip_member/$',
