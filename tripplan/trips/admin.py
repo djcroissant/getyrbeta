46317809669caa70b882b69705bee35d39c11fad
Derek Covey <derek.covey@gmail.com>
2017-12-12 11:20:26 -0800

    Run migrations and update admin

--- tripplan/trips/admin.py
+++ tripplan/trips/admin.py
@@ -1,19 +1,17 @@
 from django.contrib import admin
 
-from .models import Trip
+from .models import Trip, SunTime, Item, ItemOwner, \
+    TripMember, TripGuest, TripLocation, ItemNotification
 
-# class VehicleInline(admin.TabularInline):
-#     model = Vehicle
-#     extra = 0
+admin_models = (
+    Trip,
+    SunTime,
+    Item,
+    ItemOwner,
+    TripMember,
+    TripGuest,
+    TripLocation,
+    ItemNotification
+)
 
-class TripAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['title']}),
-        ('Date information', {'fields': ['start_date']}),
-        ('Number of nights', {'fields': ['number_nights']}),
-    ]
-    list_display = ('title', 'start_date', 'is_in_the_past')
-    list_filter = ['start_date']
-    search_fields = ['title']
-
-admin.site.register(Trip, TripAdmin)
+admin.site.register(admin_models)
