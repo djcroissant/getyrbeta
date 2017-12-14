e355377a6666dc545b938be2434e1073f32192c3
Derek Covey <derek.covey@gmail.com>
2017-12-14 15:45:18 -0800

    Install django-widget-tweaks

--- config/settings/base.py
+++ config/settings/base.py
@@ -51,6 +51,7 @@ THIRD_PARTY_APPS = [
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
     'allauth.socialaccount.providers.facebook',
+    'widget-tweaks'
 ]
 
 # Apps specific for this project go here.
