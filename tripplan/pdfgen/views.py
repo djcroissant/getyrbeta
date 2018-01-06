2d8746255ad60fc84a5ab6205ad5e8e2f6c23924
Derek Covey <derek.covey@gmail.com>
2018-01-06 12:37:20 -0800

    Refactor gear list styling to work for in-app and pdf view

--- tripplan/pdfgen/views.py
+++ tripplan/pdfgen/views.py
@@ -11,33 +11,6 @@ from easy_pdf.views import PDFTemplateView
 
 from trips.models import Trip, TripMember, TripLocation, Item
 
-def hello_world(request):
-    # Create HttpResponse object.
-    response = HttpResponse(content_type="application/pdf")
-    response["Content-Disposition"] = "attachment; filename='hello_world.pdf'"
-
-    # Create pdf object with response object as its file.
-    p = canvas.Canvas(response, pagesize=letter)
-
-    # Define variables for dimensions
-    width, height = letter
-
-    # Draw on pdf.
-    p.translate(inch, inch)
-    p.setFont("Helvetica", 14)
-    p.setStrokeColorRGB(0.2,0.5,0.3)
-    p.setFillColorRGB(1,0,1)
-    p.line(0,0,0,1.7*inch)
-    p.line(0,0,1*inch,0)
-    p.rect(0.2*inch,0.2*inch,1*inch,1.5*inch, fill=1)
-    # p.rotate(90)
-    p.setFillColorRGB(0,0,0.77)
-    p.drawString(0, 0.2*inch, "Hello world")
-
-    # Close pdf object cleanly
-    p.showPage()
-    p.save()
-    return response
 
 class TripPlanView(PDFTemplateView):
     template_name = "pdfgen/trip_plan.html"
@@ -79,3 +52,6 @@ class TripPlanView(PDFTemplateView):
             ).select_related('member')
         context['trip_members'] = trip_members
         return context
+
+class SaveTripPlanView(TripPlanView):
+    download_filename = "getyrbeta_trip_plan.pdf"
